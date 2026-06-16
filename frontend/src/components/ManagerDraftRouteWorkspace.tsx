3a186473f75fe4cf0a510174b3b5264e299050a4
Eric Austin Barber <eric.a.barber@gmail.com>
2026-06-16 13:15:28 -0700

    Use editable draft route panel in workspace

--- frontend/src/components/ManagerDraftRouteWorkspace.tsx
+++ frontend/src/components/ManagerDraftRouteWorkspace.tsx
@@ -1,18 +1,19 @@
 import type { DayPlanStop } from '../domain/dayPlans';
 import type { YardCareJob } from '../domain/jobs';
 import { ManagerAssignableJobsPanel } from './ManagerAssignableJobsPanel';
-import { ManagerDraftRoutePanel } from './ManagerDraftRoutePanel';
+import { ManagerEditableDraftRoutePanel } from './ManagerEditableDraftRoutePanel';
 
 type ManagerDraftRouteWorkspaceProps = {
   jobs: YardCareJob[];
   stops: DayPlanStop[];
   onAddJob?: (jobId: string) => void;
+  onRemoveJob?: (jobId: string) => void;
 };
 
-export function ManagerDraftRouteWorkspace({ jobs, stops, onAddJob }: ManagerDraftRouteWorkspaceProps) {
+export function ManagerDraftRouteWorkspace({ jobs, stops, onAddJob, onRemoveJob }: ManagerDraftRouteWorkspaceProps) {
   return (
     <div className="space-y-4">
-      <ManagerDraftRoutePanel stops={stops} />
+      <ManagerEditableDraftRoutePanel stops={stops} onRemoveJob={onRemoveJob} />
       <ManagerAssignableJobsPanel jobs={jobs} stops={stops} onAddJob={onAddJob} />
     </div>
   );
