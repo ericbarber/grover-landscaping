9b7891fc10f4205dc9c5dc117903ae1bcbfd756f
Eric Austin Barber <eric.a.barber@gmail.com>
2026-07-07 18:30:55 -0700

    Pass route readiness to publish action

--- frontend/src/components/ManagerDraftDayPlanActions.tsx
+++ frontend/src/components/ManagerDraftDayPlanActions.tsx
@@ -5,13 +5,25 @@ import { ManagerPublishDayPlanButton } from './ManagerPublishDayPlanButton';
 type ManagerDraftDayPlanActionsProps = {
   draftPlan: DayPlanMutationResponse;
   onUpdated: (dayPlan: DayPlanMutationResponse) => void;
+  canPublishRoute?: boolean;
+  publishDisabledReason?: string | null;
 };
 
-export function ManagerDraftDayPlanActions({ draftPlan, onUpdated }: ManagerDraftDayPlanActionsProps) {
+export function ManagerDraftDayPlanActions({
+  draftPlan,
+  onUpdated,
+  canPublishRoute,
+  publishDisabledReason,
+}: ManagerDraftDayPlanActionsProps) {
   return (
     <div className="space-y-3">
       <ManagerDraftDayPlanCard draftPlan={draftPlan} />
-      <ManagerPublishDayPlanButton draftPlan={draftPlan} onPublished={onUpdated} />
+      <ManagerPublishDayPlanButton
+        draftPlan={draftPlan}
+        onPublished={onUpdated}
+        canPublishRoute={canPublishRoute}
+        disabledReason={publishDisabledReason}
+      />
     </div>
   );
 }
